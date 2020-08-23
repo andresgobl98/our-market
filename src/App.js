ea74e5ef452fcffa4f2b20e243b6a8d3a2b7a437
Robert-Medina <robertmemo@unisabana.edu.co>
2020-08-23 11:25:18 -0500

    An update to the top bar
    Here is an update to the top bar. It includes a color and font changes.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ function App() {
     return (
         <div className="navbar-collapse">
             <ul className="ul">
-                <li className="li-left"><a href="#">Home</a></li>
-                <li className="li-left"><a href="#">Soporte</a></li>
+                <li className="li-left"><a href="#">HOME</a></li>
+                <li className="li-left"><a href="#">SOPORTE</a></li>
             </ul>
-
             <ul className="ul-right">
-                <li className="li-right"><a href="#">Salir</a></li>
+                <li className="li-right"><a href="#">SALIR</a></li>
             </ul>
 	    </div>
     );
