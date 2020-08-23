4816a30f5e40f3a761be24de4688f1cb1e4b101c
Robert-Medina <robertmemo@unisabana.edu.co>
2020-08-23 10:58:03 -0500

    Horizontal bar
    A horizontal bar at the top has been added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import './App.css';
 
 function App() {
     return (
-        <div>
-            <h1>Holi</h1>
-        </div>
+        <div className="navbar-collapse">
+            <ul className="ul">
+                <li className="li-left"><a href="#">Home</a></li>
+                <li className="li-left"><a href="#">Soporte</a></li>
+            </ul>
+
+            <ul className="ul-right">
+                <li className="li-right"><a href="#">Salir</a></li>
+            </ul>
+	    </div>
     );
 }
 
