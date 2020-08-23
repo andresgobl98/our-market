dddae64b0234e98ed9f3c50434e6b9804974aec9
Robert-Medina <robertmemo@unisabana.edu.co>
2020-08-23 16:15:47 -0500

    Slider with Negocio Objects
    Here is the slider which shows some Objects called negocios and their sectors

---
+++ src/Slider.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Slider.css';
+
+
+
+function Slider() {
+    const Negocio = props => (
+        <div className='negocio'><br></br><br></br><br></br>
+            <h1>Negocio: {props.name} </h1>
+            <h1>Sector: {props.enfoque}</h1><em></em>
+        </div>
+    );
+
+    const negociosNombres = ['Los Ocarros','Kamox','BmX4Life','Meninos'];
+    const negociosSectores = ['Insumos','Plásticos','Bicicletas','Veterinaria'];
+    let negocios = [];
+    for(let i in negociosNombres){
+        negocios.push(<Negocio name={negociosNombres[i]} 
+        enfoque={negociosSectores[i]}></Negocio>);
+    }
+    console.log(negocios);
+    /* const negocios = negociosNombres.map(name => (
+    <Negocio name={name} enfoque='XD'></Negocio>));
+    negociosSectores.map(enfoque =>(
+    <Negocio>enfoque={enfoque}</Negocio>
+    )); */
+    
+    return (
+        <div className="slider">
+			<ul>
+                 
+				<li>
+                    {negocios[0]}
+                </li>
+
+                <li>
+                    {negocios[1]}    
+                </li>
+
+                <li>
+                    {negocios[2]}
+                </li>
+
+                <li>
+                    {negocios[3]}
+                </li>
+
+			</ul>
+		</div>
+    );
+}
+
+export default Slider
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Slider from './Slider';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -8,6 +9,13 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Slider />
+  </React.StrictMode>,
+  document.getElementById('sliderIMG')
 );
 
 serviceWorker.unregister();
