3ef2928fbc8512a49d6da3d865bf2d225a2f9707
Robert-Medina <robertmemo@unisabana.edu.co>
2020-09-27 17:14:33 -0500

    Jumbotron modified
    Jumbotron has been modified

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Highlights from '../Highlights/Highlights';
 import Dashboard from '../Dashboard/Dashboard';
+import Presentation from '../Presentation/Presentation';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
+                <Presentation />
                 <Highlights />
                 <Dashboard />
             </div>
--- src/Components/Presentation/Presentation.js
+++ src/Components/Presentation/Presentation.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 
 export default class Presentation extends Component {
     render() {
         return (
-            <div>
-                <hr>XDDD</hr>
-            </div>
+            <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>
+                    This is a simple hero unit, a simple jumbotron-style component for calling
+                    extra attention to featured content or information.
+  </p>
+                <p>
+                    <Button variant="primary">Learn more</Button>
+                </p>
+            </Jumbotron>
         )
     }
 }
