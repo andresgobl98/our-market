82a703a3c1b17421862875edb006e626016f4194
Robert-Medina <robertmemo@unisabana.edu.co>
2020-09-27 17:18:34 -0500

    Update Presentation.js
    Jimbotron updated

--- src/Components/Presentation/Presentation.js
+++ src/Components/Presentation/Presentation.js
@@ -6,13 +6,15 @@ export default class Presentation extends Component {
     render() {
         return (
             <Jumbotron>
-                <h1>Hello, world!</h1>
+                <h1>¡Bienvenidos a Our-Market!</h1>
                 <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
-  </p>
+                    Our-Market es tú negocio, nuestro negocio. Aquí podrás encontrar una gran variedad de negocios
+                    de diferente tipo para cada uno de los gustos los consumidores.
+                    Puedes añadir tu negocio aquí o, si eres consumidor, buscar tu negocio favorito y contactarlo,
+                    ver su rating, algunos comentarios de otras personas e información de este.
+                </p>
                 <p>
-                    <Button variant="primary">Learn more</Button>
+                    <Button variant="primary">Contáctenos</Button>
                 </p>
             </Jumbotron>
         )
