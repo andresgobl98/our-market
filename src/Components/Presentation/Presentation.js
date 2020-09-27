31b9f82977278e182595c241305fdfecc30dc992
Robert-Medina <robertmemo@unisabana.edu.co>
2020-09-27 16:54:59 -0500

    Presentation Jubotron added
    Presentation Jubotron has been added.

---
+++ src/Components/Presentation/Presentation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Presentation extends Component {
+    render() {
+        return (
+            <div>
+                <hr>XDDD</hr>
+            </div>
+        )
+    }
+}
