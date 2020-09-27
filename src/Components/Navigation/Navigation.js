f5b1115fd6b8dc1e18931ede45e7bc1af526ecec
Robert-Medina <robertmemo@unisabana.edu.co>
2020-09-27 16:46:26 -0500

    Update Navigation.js
    Navigation bar has been updated

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -20,8 +20,10 @@ export default function Navigation() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Brand as={Link} to="/">Our Market</Navbar.Brand>
                 <Navbar.Collapse>
-                    <Nav.Link as={Link} to="/profile">Mi Perfil</Nav.Link>
-                    <Nav.Link as={Link} to="/orders">Mis Pedidos</Nav.Link>
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} to="/profile">Mi Perfil</Nav.Link>
+                        <Nav.Link as={Link} to="/orders">Mis Pedidos</Nav.Link>
+                    </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                         <Button variant="outline-success">Search</Button>
