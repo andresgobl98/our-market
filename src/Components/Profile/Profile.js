ebe686168d618fe00b04f547bdc33e0ee0f50c74
Edwin_Ramos <edwin.ale0268@gmail.com>
2020-09-28 12:15:19 -0500

    prueba

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -4,22 +4,17 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import Card from 'react-bootstrap/Card';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import axios from 'axios';
 import './Profile.css';
 
 const now = 60;
 export default class Profile extends Component {
 
-
   render() {
-    console.log(this.props.user)
     return (
 
       <Container>
+
         <Jumbotron fluid>
           <Container>
             <h1 className="Titulo">Mi perfil</h1>
@@ -52,34 +47,12 @@ export default class Profile extends Component {
             </ul>
           </Col>
         </Row>
-       <Row>
-        <Col sm="6" md="4" lg="3">
-            <Card>
-                <Card.Img className="imagen" variant="top" src={require('../../Images/computador.jpg')} />
-                <Card.Body>
-                    <Card.Title>{"Negocio 1"}</Card.Title>
-                    <Card.Text>
-                        {"Negocio de..."}
-                    </Card.Text>
-                    <span className="rating">Rating</span>
-                    <ProgressBar now={now} label={`${now}%`} />
-                    <br></br>
-                    <Dropdown as={ButtonGroup}>
-                        <Button variant="success">Ver Negocio</Button>
-
-                        <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Añadir a favoritos</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Añadir a ver más tarde</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Ocultar</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    
-                </Card.Body>
-            </Card>
-        </Col>
-        </Row>
+        
+        <Jumbotron fluid>
+          <Container>
+            <h1 className="Titulo">Mis negocios favoritos</h1>
+          </Container>
+        </Jumbotron>
       </Container>
     )
   }
